9fe7670311d3d77a615feb3f9effaa73f587594b
pieczka.marcin <pieczka.marcin@gmail.com>
2018-02-07 19:59:23 +0100

    fix

--- data_load_progress_tracker.py
+++ data_load_progress_tracker.py
@@ -1,14 +1,25 @@
+import time
+import urllib.parse
+
+from pymongo import MongoClient
+
+mongo_container = 'btc-blockchain-db'
+username = urllib.parse.quote_plus('root')
+password = urllib.parse.quote_plus('password')
+connection = MongoClient('mongodb://{}:{}@{}'.format(username, password, mongo_container))
+col = connection.bitcoin.blocks
+
 
 def fun():
-    print('be patient and wait 100s for first results')
     height = 510000
     old = col.count()
+    print('number of blocks inserted: {}'.format(old))
     time.sleep(100)
     while True:
         new = col.count()
         print('number of blocks inserted: {}, being {}% of all, and {}h left'.format(
             new, 
-            str(100 * new / height )[:4],
+            str(100 * new / height)[:4],
             str(100 * (height - new) / ((new - old) * 3600))[:4]
         ))
         old = new
