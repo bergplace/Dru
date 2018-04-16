36afd739579f894896d605b5d5ce441ca1651d96
pieczka.marcin <pieczka.marcin@gmail.com>
2018-04-16 11:48:49 +0200

    commit

--- usage-examples/performance_tests.py
+++ usage-examples/performance_tests.py
@@ -1,4 +1,6 @@
 import time
+from pprint import pprint
+
 from mongo_connection import get_db_connection
 
 
@@ -15,6 +17,14 @@ def test_getting_blocks_with_height_between(db, lower_bound, upper_bound):
     return time.time() - start_t
 
 
+def test_getting_one_block(db, height):
+    start_t = time.time()
+    block = db.blocks.find_one({'height': height})
+    pprint(block)
+    return time.time() - start_t
+
+
 if __name__ == '__main__':
     db = get_db_connection()
-    print(test_getting_blocks_with_height_between(db, 200000, 200099))
+    #print(test_getting_blocks_with_height_between(db, 200000, 200099))
+    print(test_getting_one_block(db, 299586))
