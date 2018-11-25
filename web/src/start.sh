f18ec8064e28d513783382020c6bdd898ac285f5
Marcin Pieczka <pieczka.marcin@gmail.com>
2018-11-25 23:17:32 +0100

    move web to django v0.4 - mongo connection is still not solved issue

--- web/src/start.sh
+++ web/src/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python /app/manage.py collectstatic --noinput
+#python /app/manage.py collectstatic --noinput
 python /app/manage.py migrate
 python /app/manage.py runserver 0.0.0.0:8000
--- web/src/web/settings.py
+++ web/src/web/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api'
+    'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
