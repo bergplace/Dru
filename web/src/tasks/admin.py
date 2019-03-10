1187f46b36a2cbd5541a2a675b0f4de57e1c63e1
Marcin Pieczka <pieczka.marcin@gmail.com>
2019-03-10 13:47:49 +0100

    tasks made searchable in admin page

--- web/src/tasks/admin.py
+++ web/src/tasks/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Tasks
 
-admin.site.register(Tasks)
+
+@admin.register(Tasks)
+class TaskAdmin(admin.ModelAdmin):
+    list_filter = ('id', 'status', 'email', 'received_t', 'start_t', 'end_t')
