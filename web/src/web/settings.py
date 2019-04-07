5e0df96204b5f5a13870c4ef3819adfe21d5c63b
Marcin Pieczka <pieczka.marcin@gmail.com>
2019-04-07 12:06:01 +0200

    nginx static file serving

--- web/src/web/settings.py
+++ web/src/web/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/srv/static'
 
 # Celery settings
 BROKER_URL = 'amqp://rabbit'
