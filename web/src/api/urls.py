050ce43727885abbd5872afddfcabe3298632379
Radoslaw Michalski <radoslaw.michalski@pwr.edu.pl>
2019-04-12 00:18:26 +0200

    Graph endpoint example

--- web/src/api/urls.py
+++ web/src/api/urls.py
@@ -5,8 +5,9 @@ urlpatterns = [
     path('result/<str:task_id>', views.result),
     path('register-email', views.register_email),
     path('block_by_height/<int:height>', views.get_block_by_height),
-    path('get_blocks_range/<int:start_height>/<int:end_height>', views.get_blocks_range),
-    path('get_blocks_number/<int:start_height>/<int:num_of_blocks>', views.get_blocks_number),
-    path('get_edges_range/<int:start_height>/<int:end_height>', views.get_edges_range),
+    path('get_blocks/<int:start_height>/<int:end_height>', views.get_blocks),
+    path('get_blocks_reduced/<int:start_height>/<int:end_height>', views.get_blocks_reduced),
+    path('get_edges/<int:start_height>/<int:end_height>', views.get_edges),
+    path('get_max_degree/<int:start_height>/<int:end_height>', views.get_max_degree),
     path('wait_n_seconds/<int:seconds>', views.wait_n_seconds)
 ]
