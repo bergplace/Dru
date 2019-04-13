2719cf88ddaf0b89a2b85fd942fa7bbc585c8a50
Marcin Pieczka <pieczka.marcin@gmail.com>
2019-04-13 15:42:02 +0200

    tests wait for blocks to be loaded

--- test/utils.py
+++ test/utils.py
@@ -1,4 +1,5 @@
 import json
+from time import sleep
 
 import requests
 
@@ -6,6 +7,7 @@ import requests
 class Dru:
     # until it's not in docker
     base_url = "http://localhost:8000"
+    blocks_ready = False
 
     @staticmethod
     def get(url):
@@ -20,7 +22,18 @@ class Dru:
         return Dru.base_url + url
 
     @staticmethod
+    def _wait_for_blocks():
+        height = 0
+        while height < 1000:
+            height = requests.get(Dru._url('/api/current_block_height')).json()['height']
+            print(f"currently loaded blocks: {height}")
+            sleep(1)
+        Dru.blocks_ready = True
+
+    @staticmethod
     def get_result(response):
+        if not Dru.blocks_ready:
+            Dru._wait_for_blocks()
         while True:
             result_url = response.json()['result_url']
             result = requests.get(result_url).json()
@@ -28,6 +41,7 @@ class Dru:
                 break
             if result['status'] == 'error':
                 raise TaskError
+            sleep(1)
         return result['data']
 
 
