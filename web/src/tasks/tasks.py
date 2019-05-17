9126bedbeae101f4a6ad576032026ef4b401ed55
Radoslaw Michalski <radoslaw.michalski@pwr.edu.pl>
2019-05-17 21:45:02 +0200

    Change comments to doctrings

--- web/src/tasks/tasks.py
+++ web/src/tasks/tasks.py
@@ -20,18 +20,17 @@ def get_block_by_height(height):
     return block
 
 
-#
-# get_blocks(start_height, end_height)
-#
-# Returns the list of blocks for a given range of blocks' heights.
-# The returned JSON can be used for further processing if none of the endpoints
-# is suitable for performing the requested analysis.
-# Note that this endpoint returns whole blocks, no attributes are stripped.
-#
-
 @celery_app.task
 @auto_save_result
 def get_blocks(start_height, end_height):
+    """Returns the list of blocks for a given range of blocks' heights.
+
+    The returned JSON can be used for further processing if none of the endpoints
+    is suitable for performing the requested analysis.
+    Note that this endpoint returns whole blocks, no attributes are stripped.
+
+    """
+
     if heights_are_valid(start_height, end_height):
         
         blocks = Mongo.db(os.environ['MONGODB_NAME']).blocks.find(
@@ -50,24 +49,26 @@ def get_blocks(start_height, end_height):
     else:
         return None
 
-#
-# get_blocks_reduced(start_height, end_height)
-#
-# Returns the list of blocks for a given range of blocks' heights, yet limited to the following fields:
-#  - height
-#  - time
-#  - transaction id
-#  - input addresses (or coinbase)
-#  - output addresses
-#  - value
-# As such, this is a limited version of get_blocks endpoint.
-# The returned JSON can be used for further processing if none of the endpoints
-# is suitable for performing the requested analysis.
-#
 
 @celery_app.task
 @auto_save_result
 def get_blocks_reduced(start_height, end_height):
+    """Returns the list of blocks for a given range of blocks' heights, yet with limited fields.
+
+    The returned fields are the following:
+    - height
+    - time
+    - transaction id
+    - input addresses (or coinbase)
+    - output addresses
+    - value
+
+    As such, this is a limited version of get_blocks endpoint.
+    The returned JSON can be used for further processing if none of the endpoints
+    is suitable for performing the requested analysis.
+
+    """
+
     if heights_are_valid(start_height, end_height):
 
         blocks = Mongo.db(os.environ['MONGODB_NAME']).blocks.find(
@@ -93,22 +94,22 @@ def get_blocks_reduced(start_height, end_height):
     else:
         return None
 
-#
-# get_edges(start_height, end_height)
-#
-# Returns the list of edges blocks for a given range of blocks' heights.
-# These edges can be easily imported into graph-processing libraries.
-# Fields returned:
-#  - source address
-#  - destination address
-#  - value of the transactions
-#  - block height
-#  - block time
-#
 
 @celery_app.task
 @auto_save_result
 def get_edges(start_height, end_height):
+    """Returns the list of edges blocks for a given range of blocks' heights.
+    These edges can be easily imported into graph-processing libraries.
+
+    Fields returned:
+     - source address
+     - destination address
+     - value of the transactions
+     - block height
+     - block time
+
+    """
+
     if heights_are_valid(start_height, end_height):
 
         graph = get_graph(start_height, end_height, 'false')
@@ -125,21 +126,21 @@ def get_edges(start_height, end_height):
     else:
         return None
 
-#
-# get_degree(start_height, end_height, mode)
-#
-# Returns the list of addresses and the value of degree corresponding to them
-# in the graph created from the blocks in the range [start_height, end_height].
-# The graph will be built as directed, but using mode all variants of the measure
-# can be computed:
-#  - all - total degree of the node
-#  - in - in-degree of the node
-#  - out - out-degree
-#
 
 @celery_app.task
 @auto_save_result
 def get_degree(start_height, end_height, mode):
+    """Returns the list of addresses and the value of degree corresponding to them.
+
+    The graph is created from the blocks in the range [start_height, end_height].
+    The graph will be built as directed, but using mode all variants of the measure
+    can be computed:
+     - all - total degree of the node
+     - in - in-degree of the node
+     - out - out-degree
+
+    """
+
     if heights_are_valid(start_height, end_height) and mode in ('all', 'in', 'out'):
 
         mode = mode.upper()
@@ -150,22 +151,23 @@ def get_degree(start_height, end_height, mode):
 
     else:
         return None
-#
-# get_degree_by_block(start_height, end_height, address, mode)
-#
-# Returns the list of addresses and the value of degree corresponding to them
-# in the graph created from the blocks in the range [start_height, end_height].
-# This variant computes the degree in each block separately.
-# The graph will be built as directed, but using mode all variants of the measure
-# can be computed:
-#  - all - total degree of the node
-#  - in - in-degree of the node
-#  - out - out-degree
-#
+
 
 @celery_app.task
 @auto_save_result
 def get_degree_by_block(start_height, end_height, address, mode):
+    """Returns the list of addresses and the value of degree corresponding to them.
+
+    The graph is created from the blocks in the range [start_height, end_height].
+    This variant computes the degree in each block separately.
+    The graph will be built as directed, but using mode all variants of the measure
+    can be computed:
+     - all - total degree of the node
+     - in - in-degree of the node
+     - out - out-degree
+
+    """
+
     if heights_are_valid(start_height, end_height) and mode in ('all', 'in', 'out'):
 
         mode = mode.upper()
@@ -191,21 +193,21 @@ def get_degree_by_block(start_height, end_height, address, mode):
     else:
         return None
 
-#
-# get_degree_max(start_height, end_height, mode)
-#
-# Returns the list of addresses and the value of degree corresponding to them
-# in the graph created from the blocks in the range [start_height, end_height].
-# The graph will be built as directed, but using mode all variants of the measure
-# can be computed:
-#  - all - total degree of the node
-#  - in - in-degree of the node
-#  - out - out-degree
-#
 
 @celery_app.task
 @auto_save_result
 def get_degree_max(start_height, end_height, mode):
+    """Returns the list of addresses and the value of degree corresponding to them.
+
+    The graph is created from the blocks in the range [start_height, end_height].
+    The graph will be built as directed, but using mode all variants of the measure
+    can be computed:
+     - all - total degree of the node
+     - in - in-degree of the node
+     - out - out-degree
+
+    """
+
     if heights_are_valid(start_height, end_height) and mode in ('all', 'in', 'out'):
 
         mode = mode.upper()
@@ -217,38 +219,37 @@ def get_degree_max(start_height, end_height, mode):
     else:
         return None
 
-#
-# get_betweenness(start_height, end_height, directed)
-#
-# Returns the list of addresses and the value of betweenness corresponding to them
-# in the graph created from the blocks in the range [start_height, end_height].
-# The graph can be built either as directed or undirected.
-#
-# Note: as all shortest paths have to be computed, this operation is time-consuming. Use with care.
-#
 
 @celery_app.task
 @auto_save_result
 def get_betweenness(start_height, end_height, directed):
+    """Returns the list of addresses and the value of betweenness corresponding to them.
+
+    The graph is created from the blocks in the range [start_height, end_height].
+    The graph can be built either as directed or undirected.
+    Note: as all shortest paths have to be computed, this operation is time-consuming. Use with care.
+
+    """
+
     if heights_are_valid(start_height, end_height) and directed in ('true', 'false'):
         graph = get_graph(start_height, end_height, directed == "true")
         return dict(zip(graph.vs()["name"], graph.betweenness()))
     else:
         return None
 
-#
-# get_betweenness_max(start_height, end_height, directed)
-#
-# Returns the address and the value of betwenneess in the graph created
-# from the blocks in the range [start_height, end_height].
-# The graph can be built either as directed or undirected.
-#
-# Note: as all shortest paths have to be computed, this operation is time-consuming. Use with care.
-#
 
 @celery_app.task
 @auto_save_result
 def get_betweenness_max(start_height, end_height, directed):
+    """Returns the address and the value of betwenneess in the graph created
+
+    The graph is created from the from the blocks in the range [start_height, end_height].
+    The graph can be built either as directed or undirected.
+
+    Note: as all shortest paths have to be computed, this operation is time-consuming. Use with care.
+
+    """
+
     if heights_are_valid(start_height, end_height) and directed in ('true', 'false'):
 
         graph = get_graph(start_height, end_height, directed == "true")
@@ -258,17 +259,17 @@ def get_betweenness_max(start_height, end_height, directed):
     else:
         return None
 
-#
-# get_closeness(start_height, end_height, directed)
-#
-# Returns the list of addresses and the value of closeness corresponding to them
-# in the graph created from the blocks in the range [start_height, end_height].
-# The graph can be built either as directed or undirected.
-#
 
 @celery_app.task
 @auto_save_result
 def get_closeness(start_height, end_height, directed):
+    """Returns the list of addresses and the value of closeness corresponding to them.
+
+    The graph is created from the from the blocks in the range [start_height, end_height].
+    The graph can be built either as directed or undirected.
+
+    """
+
     if heights_are_valid(start_height, end_height) and directed in ('true', 'false'):
         graph = get_graph(start_height, end_height, directed == "true")
 
@@ -277,17 +278,17 @@ def get_closeness(start_height, end_height, directed):
     else:
         return None
 
-#
-# get_closeness_max(start_height, end_height, directed)
-#
-# Returns the address and the value of the closeness in the graph created
-# from the blocks in the range [start_height, end_height].
-# The graph can be built either as directed or undirected.
-#
 
 @celery_app.task
 @auto_save_result
 def get_closeness_max(start_height, end_height, directed):
+    """Returns the address and the value of the closeness in the graph.
+
+    The graph is created from the from the blocks in the range [start_height, end_height].
+    The graph can be built either as directed or undirected.
+
+    """
+
     if heights_are_valid(start_height, end_height) and directed in ('true', 'false'):
         graph = get_graph(start_height, end_height, directed == "true")
 
@@ -296,80 +297,80 @@ def get_closeness_max(start_height, end_height, directed):
     else:
         return None
 
-#
-# get_transitivity_global(start_height, end_height)
-#
-# Returns the nodes' clustering coefficient in the graph created from the blocks in the range [start_height, end_height]
-# For global clustering coefficient value, use for get_transitivity_global.
-#
 
 @celery_app.task
 @auto_save_result
 def get_transitivity(start_height, end_height):
+    """Returns the nodes' clustering coefficient in the graph.
+
+    The graph is created from the blocks in the range [start_height, end_height]
+    For global clustering coefficient value, use for get_transitivity_global.
+
+    """
+
     if heights_are_valid(start_height, end_height):
         graph = get_graph(start_height, end_height, directed="false")
         return dict(zip(graph.vs()["name"], graph.transitivity_local_undirected(None, "zero")))
     else:
         return None
 
-#
-# get_transitivity_global(start_height, end_height)
-#
-# Returns the clustering coefficient of the graph created from the blocks in the range [start_height, end_height].
-# This value is global for the graph. For node-level clustering coefficient, use get_transitivity.
-#
 
 @celery_app.task
 @auto_save_result
 def get_transitivity_global(start_height, end_height):
+    """Returns the clustering coefficient of the graph created from the blocks in the range [start_height, end_height].
+
+    This value is global for the graph. For node-level clustering coefficient, use get_transitivity.
+
+    """
+
     if heights_are_valid(start_height, end_height):
         graph = get_graph(start_height, end_height, directed="false")
         return graph.transitivity_undirected()
     else:
         return None
 
-#
-# get_diameter(start_height, end_height, directed)
-#
-# Returns the diameter of the graph created from the blocks in the range [start_height, end_height].
-# The graph can be considered as directed or undirected.
-#
 
 @celery_app.task
 @auto_save_result
 def get_diameter(start_height, end_height, directed):
+    """Returns the diameter of the graph created from the blocks in the range [start_height, end_height].
+
+    The graph can be considered as directed or undirected.
+
+    """
+
     if heights_are_valid(start_height, end_height) and directed in ('true', 'false'):
         graph = get_graph(start_height, end_height, directed)
         return graph.diameter()
     else:
         return None
 
-#
-# get_density(start_height, end_height, directed, loops)
-#
-# Returns the density of the graph created from the blocks in the range [start_height, end_height].
-# The graph can be considered as directed or undirected.
-#
 
 @celery_app.task
 @auto_save_result
 def get_density(start_height, end_height, directed, loops):
+    """Returns the density of the graph created from the blocks in the range [start_height, end_height].
+
+    The graph can be considered as directed or undirected.
+    """
+
     if heights_are_valid(start_height, end_height) and directed in ('true', 'false') and loops in ('true', 'false'):
         graph = get_graph(start_height, end_height, directed)
         return graph.density()
     else:
         return None
 
-#
-# are_connected(start_height, end_height, address1, address2, directed)
-#
-# Returns true/false information whether two addresses are connected within a given range of blocks.
-# If any of those does not exist in the graph, None will be returned.
-#
 
 @celery_app.task
 @auto_save_result
 def are_connected(start_height, end_height, address1, address2, directed):
+    """Returns true/false information whether two addresses are connected within a given range of blocks.
+
+    If any of these addresses does not exist in the graph, None will be returned.
+
+    """
+
     if heights_are_valid(start_height, end_height) and directed in ('true', 'false'):
         graph = get_graph(start_height, end_height, directed)
         if address1 in graph.vs()["name"] and address2 in graph.vs()["name"]:
@@ -384,18 +385,18 @@ def are_connected(start_height, end_height, address1, address2, directed):
     else:
         return None
 
-#
-# get_transactions_value(start_height, end_height, address1, address2)
-#
-# Returns the count and total value of transactions between two addresses in the graph
-# created from the blocks in the range [start_height, end_height].
-#
-# If any of those does not exist in the graph, None will be returned.
-#
 
 @celery_app.task
 @auto_save_result
 def get_transactions_value(start_height, end_height, address1, address2):
+    """Returns the count and total value of transactions between two addresses in the graph.
+
+    The graph is created from the blocks in the range [start_height, end_height].
+
+    If any of these addresses does not exist in the graph, None will be returned.
+
+    """
+
     if heights_are_valid(start_height, end_height):
         graph = get_graph(start_height, end_height, directed="true")
         if address1 in graph.vs()["name"] and address2 in graph.vs()["name"]:
