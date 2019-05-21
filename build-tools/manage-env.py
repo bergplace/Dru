4d943b5d400e942812eea4a73c1dc9ca4daaecd0
Marcin Pieczka <pieczka.marcin@gmail.com>
2019-05-21 19:58:07 +0200

    Set placeholder ssl files in manage_vars

--- build-tools/manage-env.py
+++ build-tools/manage-env.py
@@ -27,7 +27,8 @@ else:
     vars['cryptocurrency_host'] = ''
 
 vars['web_port'] = '8000' if vars['debug'] == 'true' else '80'
-
+vars['web_ssl_key_path'] = './web/conf/placeholder.key'
+vars['web_ssl_cert_path'] = './web/conf/placeholder.cert'
 
 with open(".env", "w") as env_file:
     env_file.write("".join(["{}={}\n".format(k, v) for k, v in vars.items()]))
